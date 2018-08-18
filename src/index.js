d9c62eddd0ee1ff1529c34ff90f15602c5c4f325
Andrei Volchenko <volchenko.andrei@gmail.com>
2018-08-19 01:59:26 +0300

    ### Game state modifications
    Now we need to implement a function which will switch a current player. Let's do this with `XOR` operator. ([how xor works](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators#Bitwise_XOR)).

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ const GameState = {
     currentPlayer: 0,
     field: Array.from({ length: 9 }).fill(-1),
 }
+
+function changeCurrentPlayer(gameState) {
+    gameState.currentPlayer = 1 ^ gameState.currentPlayer;
+}
