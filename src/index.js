b3467e5ed1881d3cc48799bc64bdb71d0b30e72d
Andrei Volchenko <volchenko.andrei@gmail.com>
2018-08-19 02:43:36 +0300

    ### Win
    The next thing we need to handle is a "win" state.
    Lets add helper variables which will contain array indices by rows:

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ const GameState = {
     field: Array.from({ length: 9 }).fill(-1),
 }
 
+const Rows = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
 function changeCurrentPlayer(gameState) {
     gameState.currentPlayer = 1 ^ gameState.currentPlayer;
 }
