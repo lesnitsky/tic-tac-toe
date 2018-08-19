6aaa1ceeff3d80db6826c390e0aa9be2d8e34ec8
Andrei Volchenko <volchenko.andrei@gmail.com>
2018-08-19 04:32:02 +0300

    Now let's create `render` function which will visualize the game state

---
+++ src/renderer.js
@@ -0,0 +1,12 @@
+/**
+ * @typedef GameState
+ * @property {Number} currentPlayer
+ * @property {Array<number>} field
+ *
+ * @param {HTMLCanvasElement} canvas
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {GameState} gameState
+ */
+export function draw(canvas, ctx, gameState) {
+
+}
