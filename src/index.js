cc61e113b806c6478b18bd133d62b17ac9f1b802
Andrei Volchenko <volchenko.andrei@gmail.com>
2018-08-19 04:21:38 +0300

    and get a 2d context

--- src/index.js
+++ src/index.js
@@ -19,3 +19,5 @@ const canvas = document.querySelector('canvas');
 const size = Math.min(document.body.offsetHeight, document.body.offsetWidth);
 canvas.width = size;
 canvas.height = size;
+
+const ctx = canvas.getContext('2d');
